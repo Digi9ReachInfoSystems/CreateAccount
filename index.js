2360cc5ced9d122040fd2330eeb68fc4bb3c2744
Rajat saxena <62472831+rajatsaxena7@users.noreply.github.com>
2024-09-26 16:19:35 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const express = require('express');
-const admin = require('./firebaseConfig'); // Import initialized Firebase Admin SDK
+const express = require("express");
+const admin = require("./firebaseConfig"); // Import initialized Firebase Admin SDK
 
 const app = express();
 app.use(express.json());
@@ -8,11 +8,27 @@ const db = admin.firestore();
 
 const FieldValue = admin.firestore.FieldValue;
 
-app.post('/createUser', async (req, res) => {
-  const { userId, email, password, displayName, user_role, phoneNumber, address } = req.body;
+app.post("/createUser", async (req, res) => {
+  const {
+    userId,
+    email,
+    password,
+    displayName,
+    user_role,
+    phoneNumber,
+    address,
+  } = req.body;
 
-  if (!userId || !email || !password || !displayName || !user_role || !phoneNumber || !address) {
-    return res.status(400).send({ message: 'Missing required fields' });
+  if (
+    !userId ||
+    !email ||
+    !password ||
+    !displayName ||
+    !user_role ||
+    !phoneNumber ||
+    !address
+  ) {
+    return res.status(400).send({ message: "Missing required fields" });
   }
 
   try {
@@ -27,23 +43,27 @@ app.post('/createUser', async (req, res) => {
     // await admin.auth().setCustomUserClaims(userRecord.uid, { user_role: user_role });
 
     // Store user data in Firestore, using user_role from req.body
-    await db.collection('users').doc(userRecord.uid).set({
+    await db.collection("users").doc(userRecord.uid).set({
       email: userRecord.email,
       display_name: userRecord.displayName,
       user_role: user_role,
       userId: userId,
       phoneNumber: phoneNumber, // Provided phone number
-      address: address, 
+      address: address,
       createdAt: "1108",
     });
 
-    res.status(201).send({ message: 'User created successfully', userId: userRecord.uid });
+    res
+      .status(201)
+      .send({ message: "User created successfully", userId: userRecord.uid });
   } catch (error) {
-    console.error('Error creating user:', error);
-    res.status(500).send({ message: 'Error creating user', error: error.message });
+    console.error("Error creating user:", error);
+    res
+      .status(500)
+      .send({ message: "Error creating user", error: error.message });
   }
 });
 
 app.listen(3000, () => {
-  console.log('Server running on port 3000');
+  console.log("Server running on port 3000");
 });
